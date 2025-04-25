7f11bc418070980b5dc57e007e80177ebce14499
TJOEUNIS <HwaEVE@gmail.com>
2025-04-25 16:05:13 +0900

    feat : 관리자 로그인 구현

--- src/main/java/Etf/admin/AdminService.java
+++ src/main/java/Etf/admin/AdminService.java
@@ -1,13 +1,34 @@
 package Etf.admin;
 
+import Etf.admin.dto.AdminLoginRequest;
+import Etf.admin.dto.AdminLoginResponse;
+import Etf.loginUtils.JwtProvider;
+import Etf.loginUtils.SecurityUtils;
+import Etf.user.User;
+import Etf.user.dto.CreateUserRequest;
+import Etf.user.dto.UserLoginRequest;
+import Etf.user.dto.UserLoginResponse;
+import Etf.user.dto.UserResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
+
+@RequiredArgsConstructor
 @Service
 public class AdminService {
 
     private final AdminRepository adminRepository;
+    private final JwtProvider jwtProvider;
 
-    public AdminService(AdminRepository adminRepository) {
-        this.adminRepository = adminRepository;
+    public AdminLoginResponse login(AdminLoginRequest loginRequest) {
+        Admin admin = adminRepository.findByLoginId(loginRequest.loginId()).orElseThrow(
+                () -> new NoSuchElementException("찾을 수 없는 관리자 id : " + loginRequest.loginId()));
+
+        admin.findByPassword(loginRequest.password());
+
+        return new AdminLoginResponse(jwtProvider.createToken(admin.getLoginId()));
     }
+
+
 }
