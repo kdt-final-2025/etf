16026086a9cb8dd4b8ba9663d9255768fad1fe50
TJOEUNIS <HwaEVE@gmail.com>
2025-04-25 11:37:38 +0900

    feat : user 테이블명 수정

--- src/main/java/Etf/user/User.java
+++ src/main/java/Etf/user/User.java
@@ -1,14 +1,13 @@
 package Etf.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
 
 }
