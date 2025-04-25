42df078b4f7dad9430cfe9a29085e9c473c0fbde
TJOEUNIS <HwaEVE@gmail.com>
2025-04-25 17:44:40 +0900

    refactor : uri @RequestMapping으로 겹치는 부분(/api/v1)

--- src/main/java/Etf/admin/AdminController.java
+++ src/main/java/Etf/admin/AdminController.java
@@ -5,15 +5,17 @@ import Etf.admin.dto.AdminLoginResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping(value = "/api/v1/admin")
 @RequiredArgsConstructor
 @RestController
 public class AdminController {
 
     private final AdminService adminService;
 
-    @PostMapping("/api/v1/admin/login")
+    @PostMapping("/login")
     public AdminLoginResponse login(@RequestBody AdminLoginRequest loginRequest) {
         return adminService.login(loginRequest);
     }
