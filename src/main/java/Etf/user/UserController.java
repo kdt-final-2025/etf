466f7d1216109aed488e987f16f9063c0ac75020
TJOEUNIS <HwaEVE@gmail.com>
2025-04-25 17:44:40 +0900

    refactor : uri @RequestMapping으로 겹치는 부분(/api/v1)

--- src/main/java/Etf/user/UserController.java
+++ src/main/java/Etf/user/UserController.java
@@ -6,38 +6,39 @@ import jakarta.persistence.Id;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@RequestMapping(value = "/api/v1/users")
 @RequiredArgsConstructor
 @RestController
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/api/v1/users")
+    @PostMapping()
     public UserResponse create(@RequestBody CreateUserRequest userRequest) {
         return userService.create(userRequest);
     }
 
-    @PostMapping("/api/v1/users/login")
+    @PostMapping("/login")
     public UserLoginResponse login(@RequestBody UserLoginRequest loginRequest) {
         return userService.login(loginRequest);
     }
 
-    @PatchMapping("/api/v1/users")
+    @PatchMapping()
     public UserUpdateResponse profileUpdate(@LoginMember String auth, @RequestBody UserUpdateRequest updateRequest) {
         return userService.profileUpdate(auth,updateRequest);
     }
 
-    @DeleteMapping("/api/v1/users")
+    @DeleteMapping()
     public UserDeleteResponse delete(@LoginMember String auth) {
         return userService.delete(auth);
     }
 
-    @PostMapping("/api/v1/users/me/password")
+    @PostMapping("/me/password")
     public UserPasswordResponse passwordUpdate(@LoginMember String auth, @RequestBody UserPasswordRequest passwordRequest) {
         return userService.passwordUpdate(auth, passwordRequest);
     }
 
-    @GetMapping("/api/v1/users/{userId}")
+    @GetMapping("/{userId}")
     public MypageResponse findByUser(@LoginMember String auth, @PathVariable Long userId) {
         return userService.findByUser(auth, userId);
     }
