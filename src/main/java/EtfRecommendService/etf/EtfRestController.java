95000b06169e7714f367a4acecf2ea238f9c6980
jun <mmy519@naver.com>
2025-04-29 16:13:27 +0900

    fix : SortOrder defaultValue = ""를 required = false 로 변경 (enum에 ""빈 문자열 들어오면 바인딩 에러 나기 때문에 없으면 null로)

--- src/main/java/EtfRecommendService/etf/EtfRestController.java
+++ src/main/java/EtfRecommendService/etf/EtfRestController.java
@@ -16,11 +16,12 @@ public class EtfRestController {
 
     private final EtfService etfService;
 
+    //sortorder "" 변경 : Enum은 "" (빈 문자열)이 들어오면 바인딩 에러가 나기 때문, 없으면 null로 오게
     @GetMapping("/etfs")
     public ResponseEntity<EtfResponse> read(@RequestParam(defaultValue = "1") int page,
                                            @RequestParam(defaultValue = "20") int size,
                                            @RequestParam(required = false) Theme theme,
-                                           @RequestParam(defaultValue = "") SortOrder sortOrder) {
+                                           @RequestParam(required = false) SortOrder sortOrder) {
         Pageable pageable = PageRequest.of(page - 1, size);
         EtfResponse etfResponse = etfService.readAll(pageable, theme, sortOrder);
         return ResponseEntity.status(HttpStatus.OK).body(etfResponse);
