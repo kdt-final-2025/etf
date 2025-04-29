f140e3a2d97deb468567ec6a821fa95033ebf0a6
chumin-yeong <chu35my@gmail.com>
2025-04-29 13:24:37 +0900

    fix: sse연결이 안되어 있을 때 알림을 저장할 생성자 추가

--- src/main/java/EtfRecommendService/etf/notification/Notification.java
+++ src/main/java/EtfRecommendService/etf/notification/Notification.java
@@ -6,7 +6,6 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -16,15 +15,17 @@ public class Notification {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NonNull
     private String userId; // 알림 받을 사용자 ID
 
-    @NonNull
     private String message;
 
-    @NonNull
     private LocalDateTime expiredTime;
 
     private boolean isRead = false;
 
+    public Notification(String userId, String message, LocalDateTime expiredTime) {
+        this.userId = userId;
+        this.message = message;
+        this.expiredTime = expiredTime;
+    }
 }
