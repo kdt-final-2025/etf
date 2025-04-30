62181a3f28f6ee0ef028a35e4771f155bfc18f74
wjdgowns <wjdgowns4414@gmail.com>
2025-04-30 13:57:07 +0900

    잘못 생성되어 있던 신고 관련 코드 삭제

--- src/main/java/EtfRecommendService/report/CommentReportRepository.java
+++
@@ -1,6 +0,0 @@
-package EtfRecommendService.report;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentReportRepository extends JpaRepository<CommentReport, Long> {
-}
--- src/main/java/EtfRecommendService/report/ReplyReportRepository.java
+++
@@ -1,6 +0,0 @@
-package EtfRecommendService.report;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReplyReportRepository extends JpaRepository<ReplyReport, Long> {
-}
