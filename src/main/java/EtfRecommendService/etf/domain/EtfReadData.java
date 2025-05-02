8d44d8fe820018424f5eead6cbf9398c5a141f76
chumin-yeong <chu35my@gmail.com>
2025-05-02 19:55:36 +0900

    fix: getter, 기본 생성자, 전체 생성자 추가

--- src/main/java/EtfRecommendService/etf/domain/EtfReadData.java
+++ src/main/java/EtfRecommendService/etf/domain/EtfReadData.java
@@ -5,7 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 @Entity
 public class EtfReadData {
     @Id
