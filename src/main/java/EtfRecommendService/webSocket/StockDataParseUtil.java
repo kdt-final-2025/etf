87692bafb86d06ef8e125b70fe2f093c93013f6b
jun <mmy519@naver.com>
2025-05-08 13:31:34 +0900

    feat : dayOverDayChange 전일 대비 가격 추가

--- src/main/java/EtfRecommendService/webSocket/StockDataParseUtil.java
+++ src/main/java/EtfRecommendService/webSocket/StockDataParseUtil.java
@@ -4,26 +4,28 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 public class StockDataParseUtil {
     public static StockPriceData parseFromDelimitedFields(String[] field){
-        if (field.length < 5){
+        if (field.length < 14){
             throw new IllegalArgumentException("필드 부족");
         }
         return StockPriceData.builder()
-                .code(field[0])
-                .price(Double.parseDouble(field[1]))
-                .sign(field[2])
-                .rate(Double.parseDouble(field[3]))
-                .volume(Long.parseLong(field[4]))
+                .stockCode(field[0])
+                .currentPrice(Double.parseDouble(field[2]))
+                .dayOverDaySign(field[3])
+                .dayOverDayChange(Integer.parseInt(field[4]))
+                .dayOverDayRate(Double.parseDouble(field[5]))
+                .accumulatedVolume(Long.parseLong(field[13]))
                 .build();
     }
 
     //json 메세지 -> stockpricedata로 변환
     public static StockPriceData parseFromJson(JsonNode json){
         return StockPriceData.builder()
-                .code(json.path("code").asText())
-                .price(json.path("price").asDouble())
-                .sign(json.path("sign").asText())
-                .rate(json.path("rate").asDouble())
-                .volume(json.path("volume").asLong())
+                .stockCode(json.path("stockCode").asText())
+                .currentPrice(json.path("currentPrice").asDouble())
+                .dayOverDaySign(json.path("dayOverDaySign").asText())
+                .dayOverDayChange(json.path("dayOverDayChange").asInt())
+                .dayOverDayRate(json.path("dayOverDayRate").asDouble())
+                .accumulatedVolume(json.path("accumulatedVolume").asLong())
                 .build();
     }
 }
