b608118704190ef811c9eed070ab9ded0d78c8e2
jun <mmy519@naver.com>
2025-05-23 12:17:24 +0900

    fix :한국투자 restAPI 요청 부분, FrontendWebSocketHandler 삭제

--- src/main/java/EtfRecommendService/restAPI/SnapshotRepository.java
+++
@@ -1,6 +0,0 @@
-package EtfRecommendService.restAPI;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SnapshotRepository extends JpaRepository<SnapshotEntity,Long> {
-}
